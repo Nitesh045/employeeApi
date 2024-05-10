b88e336f097cffca9a8a42accc5d171c6347f199
Nitesh Yadav <niteshyadav604@gmail.com>
2024-05-10 18:30:05 +0530

    adding

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500"); // Update this with your actual origin
+    res.header("Access-Control-Allow-Origin"); // Update this with your actual origin
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -28,8 +28,8 @@ const tablerouter= require('./routes/tableRoutes')
 app.use(tablerouter)
 
 
-// const userdata= require('./routes/userRoutes');
-// app.use(userdata)
+const userdata= require('./routes/userRoutes');
+app.use(userdata)
 
 app.listen(port,()=>{
     console.log(`server listining at ${port}`)
--- controller/userController.js
+++ controller/userController.js
@@ -1,75 +1,75 @@
-// const express = require('express');
-// const router = express.Router();
-// // import userDetails from '../Schema/user'
-// const userDetails = require('../Schema/user');
+const express = require('express');
+const router = express.Router();
+// import userDetails from '../Schema/user'
+const userDetails = require('../Schema/user');
 
-// const bcrypt = require('bcrypt');
-// const jwt = require('jsonwebtoken');
-
-
-// const maxAge = 3 * 24 * 60 * 60;
-// const createToken = (id) => {
-//     return jwt.sign({ id }, 'qwertyuioplkjhgfdsazxcvbnm', {
-//         expiresIn: maxAge
-//     });
-// };
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-// module.exports.singup_post = async (req, res, next) => {
-//     const { firstName, lastName, email, password } = req.body;
-//     let existingUser;
-//     try {
-//         existingUser = await userDetails.findOne({ email: email });
-//     } catch (error) {
-//         console.log(error);
-//     }
-//     if (existingUser) {
-//         return res
-//             .status(400).json({ message: "teacher alredy " })
-//     }
-
-//     const hasingPassword = bcrypt.hashSync(password, 10);
-//     try {
-
-//         const iteams = new userDetails({
-//             firstName,
-//             lastName,
-//             email,
-//             password: hasingPassword,
-//         });
-
-//         const userfinalData = await iteams.save();
-//         const token = createToken(userDetails._id);
-//         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
-//         console.log('data addees')
-//     } catch (e) {
-//         console.log(e)
-//     }
-//     return res.status(201).json({ message: "data added" })
-// };
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'qwertyuioplkjhgfdsazxcvbnm', {
+        expiresIn: maxAge
+    });
+};
 
 
+module.exports.singup_post = async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await userDetails.findOne({ email: email });
+    } catch (error) {
+        console.log(error);
+    }
+    if (existingUser) {
+        return res
+            .status(400).json({ message: "teacher alredy " })
+    }
 
-// module.exports.login_post = async (req, res, next) => {
-//     const { email, password } = req.body;
-//     let existingUser;
-//     try {
-//         existingUser = await userDetails.findOne({ email: email })
-//     } catch (error) {
-//         console.log(error)
-//     }
+    const hasingPassword = bcrypt.hashSync(password, 10);
+    try {
 
-//     if (!existingUser) {
-//         return res.status(400).json({ message: "user not found singup" })
-//     }
+        const iteams = new userDetails({
+            firstName,
+            lastName,
+            email,
+            password: hasingPassword,
+        });
 
-//     const CorrectPassword = bcrypt.compareSync(password, existingUser.password);
+        const userfinalData = await iteams.save();
+        const token = createToken(userDetails._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 })
+        console.log('data addees')
+    } catch (e) {
+        console.log(e)
+    }
+    return res.status(201).json({ message: "data added" })
+};
 
-//     if (!CorrectPassword) {
-//         return res.status(400).json({ message: "password is incorrect" });
-//     }
 
-//     const token = createToken(existingUser._id);
-//     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-//     return res.status(200).json({ message: "loged in", iteams: existingUser, token });
-// }
+
+module.exports.login_post = async (req, res, next) => {
+    const { email, password } = req.body;
+    let existingUser;
+    try {
+        existingUser = await userDetails.findOne({ email: email })
+    } catch (error) {
+        console.log(error)
+    }
+
+    if (!existingUser) {
+        return res.status(400).json({ message: "user not found singup" })
+    }
+
+    const CorrectPassword = bcrypt.compareSync(password, existingUser.password);
+
+    if (!CorrectPassword) {
+        return res.status(400).json({ message: "password is incorrect" });
+    }
+
+    const token = createToken(existingUser._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    return res.status(200).json({ message: "loged in", iteams: existingUser, token });
+}
